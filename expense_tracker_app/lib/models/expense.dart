6bd717adb27fb341cd327861a457310642ce70e4
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-06-04 17:53:39 -0300

    create a expense tracker app proj

---
+++ expense_tracker_app/lib/models/expense.dart
@@ -0,0 +1,9 @@
+class Expense {
+  const Expense(
+      {required this.title, required this.amount, required this.date});
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+}
