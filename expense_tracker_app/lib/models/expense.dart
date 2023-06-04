a99ac6263c9c76303fc35b0a999bc8913a5c2fd5
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-06-04 18:02:41 -0300

    uuid

--- expense_tracker_app/lib/models/expense.dart
+++ expense_tracker_app/lib/models/expense.dart
@@ -1,6 +1,12 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
 class Expense {
-  const Expense(
-      {required this.title, required this.amount, required this.date});
+  Expense({required this.title, required this.amount, required this.date})
+      : id = uuid.v4();
 
   final String id;
   final String title;
